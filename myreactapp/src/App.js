2e6b03b154b66170c39bcbb0166de8a390d513b7
mischmi2023 <mis.chmi2023@gmail.com>
2025-12-02 20:51:14 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = <h1>Mike</h1>
+  const name = <h1> Mike </h1>
+  const age = <h2> 41 </h2>
+  const email = <h2> migz.dv8@gmail.com </h2>
   return (
     <div className="App">
      {name}
+     {age}
+     {email}
     </div>
   );
 }
