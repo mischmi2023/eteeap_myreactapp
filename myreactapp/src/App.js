b76c207567399e76512c733a0112fc465593d402
mischmi2023 <mis.chmi2023@gmail.com>
2025-12-02 21:10:31 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = <h1> Mike </h1>
-  const age = <h2> 41 </h2>
-  const email = <h2> migz.dv8@gmail.com </h2>
-  const user = (div>
-     {name}
-     {age}
-     {email}
-    </div>
-  );
   return (
     <div className="App">
-      {user}
-      {user}
-      {user}  
+      <User />
+      <User />
+      <User />
     </div>
-
-    
   );
 }
+  const User = () => {
+    return (
+    <div>
+      <h1> Mike </h1>
+      <h2> 41 </h2>
+      <h2> migz.dv8@gmail.com </h2>
+    </div>
+
+);
+
+}
+   
 
 export default App;
