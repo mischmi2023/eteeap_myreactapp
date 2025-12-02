a809040e84737248852417afa806a240532a9b86
mischmi2023 <mis.chmi2023@gmail.com>
2025-12-02 20:58:12 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -5,12 +5,20 @@ function App() {
   const name = <h1> Mike </h1>
   const age = <h2> 41 </h2>
   const email = <h2> migz.dv8@gmail.com </h2>
-  return (
-    <div className="App">
+  const user = (div>
      {name}
      {age}
      {email}
     </div>
+  );
+  return (
+    <div className="App">
+      {user}
+      {user}
+      {user}  
+    </div>
+
+    
   );
 }
 
