f293bdb966e67f2c04e7b8e86bff2e49ab1cbfa9
mischmi2023 <mis.chmi2023@gmail.com>
2025-12-14 20:28:19 +0800

    app.css

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,22 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Job salary ={100000} position ="Senior SDE" company ="Amazon" />
-      <Job salary ={80000} position ="Junior SDE" company ="Google" />
-      <Job salary ={60000} position ="Project Manager" company ="Facebook" />
-    </div>
-  );
-}
-
-  const Job = (props) => {
-    return (
-    <div>
-      <h1> {props.salary} </h1>
-      <h2> {props.position} </h2>
-      <h2> {props.company} </h2>
-    </div>
-
+   <div className="App">
+    <h1 className="name"> Mike </h1>
+    <h1 className='title-1'> Implementing CSS in React </h1>
+   </div>
 );
 
 }
