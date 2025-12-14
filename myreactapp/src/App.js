a975475c8a9377938f448d8ef547a86f060dc1f4
mischmi2023 <mis.chmi2023@gmail.com>
2025-12-14 22:20:21 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,15 +2,9 @@ import './App.css';
 
 function App() {
   
-  const age = 15;
+  const age = 19;
+  return <div className="App"> {age >= 18 ? <h1> Over Age </h1> :
+    <h1> Under Age </h1>}</div>
 
-  if (age >= 18) {
-    return <h1> Over Age </h1>;
-  }
-  else {
-    return <div className="App">
-      <h1> Under Age </h1>
-      </div>
-  }
 }
 export default App;
