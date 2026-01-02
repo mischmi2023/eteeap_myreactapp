5bcdcf96007aee40641f4867e43defddbc39db42
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-02 20:58:49 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
   
   const age = 19;
-  const isGreen = false;
+  const isGreen = true;
   return <div className="App"> {age >= 18 ? <h1> Over Age </h1> :
     <h1> Under Age </h1>}
-    <h1 style={{ color: isGreen ? "green" : "red"}}> This has color</h1>
+    <h1 style={{ color: isGreen ? "green" : "red"}}> This has color </h1>
+    {isGreen && <button> This is a Button </button>}
     </div>;
-
 }
 export default App;
