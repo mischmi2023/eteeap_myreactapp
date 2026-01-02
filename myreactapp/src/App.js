d5a1d93d335b40286a9c1af1222779b23c0166c8
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-02 21:09:59 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   const names = ["Madz", "Rap", "Sean", "Edel", "Pol", "Jeus", "Joms"]
+
+
+
   return <div className="App">
-    <h1> {names[0]}</h1>
+   {names.map((name, key) => { 
+    return <h1> {name} </h1>
+   })}
     </div>;
 }
 export default App;
