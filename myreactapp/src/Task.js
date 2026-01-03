498d227394307b0e7414fa300c723529cdd2c903
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-03 20:52:47 +0800

    Update
    App.css
    Task.js

--- myreactapp/src/Task.js
+++ myreactapp/src/Task.js
@@ -4,8 +4,7 @@ export const Task = (props) => {
         style={{ backgroundColor: props.completed ? "green" : "white"}}>
         <h1>{props.taskName}</h1>
         <button onClick={() => props.completeTask(props.id)}> Complete </button>
-        <button onClick={() => props.deleteTask(props.id)}> X </button>
-           
+        <button onClick={() => props.deleteTask(props.id)}> X </button>          
         </div>
     );
 };
