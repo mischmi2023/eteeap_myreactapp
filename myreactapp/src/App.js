55203b82375aa5022cf39e5b6b670386568ae74a
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-03 16:08:09 +0800

    Update App.js
    react CRUD
    create, read, update, & delete

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,28 +1,44 @@
 import './App.css';
 import { useState } from "react"; //to create a state
+//CRUD
 
 function App() {
-const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState([]); //Array with no initial value
+  const [newTask, setNewtask] = useState("");
 
-const increase = () => {
-  setCount(count + 1);
-}
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
 
-const decrease = () => {
-  setCount(count - 1);
-}
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+  };
 
-const setToZero = () => {
-  setCount(0);
-}
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
 
-return (
-  <div className="App">
-    <button className="glass-button" onClick={increase}> Increase </button>
-    <button className="glass-button" onClick={decrease}> Decrease </button>
-    <button className="glass-button" onClick={setToZero}> Set to Zero </button>
-    <h1> {count} </h1>
-  </div>
- );
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button className="glass-button" onClick={addTask} > Add Task </button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <div className="task">
+              <h1>{task.taskName}</h1>
+              <button onClick={() => deleteTask(task.id)}> X </button>
+              </div>
+          );
+        })} 
+      </div>
+    </div>
+  );
 }
 export default App;
