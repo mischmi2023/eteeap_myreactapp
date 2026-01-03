29a7fe396e2802ab2dfd4bc9460760388c6b11b2
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-03 14:27:38 +0800

    show/hide text

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 import { useState } from "react"; //to create a state
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-};
-return (
-  <div className="App">
-    <div className = "inpt">
-     <input type="text" onChange={handleInputChange} />
-    </div>
-    <div className = "mirror">
-     {inputValue}
-  </div>
+  const [showText, setShowText] = useState(true);
 
-  </div>
+  return (
+    <div className="App">
+      <button 
+        onClick={() => {
+          // alert("Hey");
+          setShowText(!showText)
+        }}
+      >
+        {" "}
+        Show/Hide{" "}
+      </button>
+      {showText === true && <h1> Hello World </h1>}
+    </div>
  );
 }
 export default App;
