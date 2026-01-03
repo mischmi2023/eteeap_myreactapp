d541a079d4aa828629291413e71ad107718ccf1c
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-03 13:59:20 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,17 +2,14 @@ import './App.css';
 import { useState } from "react"; //to create a state
 
 function App() {
- //let numbr = 0;
- const [numbr, setNumbr] = useState(1); // creating an initial state for numbr 0.
- const increaseNumbr = () => {
-  // age = age + 1;
-  // console.log(age);
-  setNumbr(numbr + 1); //react will rerender the whole UI to increase the number.
- };
- return (
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+};
+return (
   <div className="App">
-    {numbr}
-    <button onClick={increaseNumbr}> Increase Number </button>
+    <input type="text" onChange={handleInputChange} />
+    {inputValue}
   </div>
  );
 }
