b4c428c52082b5ef992134fe7c3286d03d5f7a4f
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-12 20:44:54 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Axios from "axios";
-import { useState, useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [catFact, setCatFact] = useState("");
