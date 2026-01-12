0ec819d9690d92426bb72996bf3d644327f55760
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-12 21:21:09 +0800

    App.css

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -4,24 +4,26 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [name, setName] = useState("");
-  const [predictedAge, setPredictedAge] = useState(0);
+  const [predictedAge, setPredictedAge] = useState(null); // replaced 0 by null
   const fetchData = () => {
     Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
       console.log(res.data);
-      setPredictedAge(res.data.age);
+      setPredictedAge(res.data); // remove .age toget all data objects
     });
   };
 
   return (
     <div className="App">
-      <input 
+      <input
         placeholder="Ex. Mike"
         onChange={(event) => {
           setName(event.target.value);
         }}
       />
-      <button onClick ={fetchData}> Predict Age </button>
-      <h3> Predict Age: {predictedAge} </h3>
+      <button onClick={fetchData}> Predict Age </button>
+      <h3> Name: {predictedAge?.name} </h3>
+      <h3> Predicted Age: {predictedAge?.age} </h3>
+      <h3> Count: {predictedAge?.count} </h3>
     </div>
   );
 }
