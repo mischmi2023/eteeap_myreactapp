977cf9ae7e61e649dee02c9cc0e6032465dc5dab
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-12 21:08:48 +0800

    update App.js
    update package.json
    update App.js
    update index.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -3,21 +3,25 @@ import Axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [catFact, setCatFact] = useState("");
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact);
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(0);
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      console.log(res.data);
+      setPredictedAge(res.data.age);
     });
   };
 
-  useEffect(() => {
-   fetchCatFact(); // calling only the function name 
-  }, []);
-
   return (
     <div className="App">
-      <button onClick={fetchCatFact}> Generate Cat Fact </button>
-      <p>{catFact}</p>
+      <input 
+        placeholder="Ex. Mike"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick ={fetchData}> Predict Age </button>
+      <h3> Predict Age: {predictedAge} </h3>
     </div>
   );
 }
--- myreactapp/src/index.js
+++ myreactapp/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Important: import from 'react-dom/client'
+import './index.css';
 import App from './App';
 
-const root = React.DOM.createRoot(document.getElementById('root'));
+// Create a root element using ReactDOM.createRoot
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode> removed to prevent fetching data twice used in useEffect.
-  <App/>
-  /* </React.StrictMode> */
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
