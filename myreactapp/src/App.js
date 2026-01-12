f1921e0882bb297bd2a3065e30889bbefab3154f
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-12 20:15:10 +0800

    App.js
    preventing from fetching infinite data by adding an empty array and preventing fetching data twice from API

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Axios from 'axios';
 import { useState } from "react";
 
-// This will generate a finite CatFacts.
 function App(){
   const [catFact, setCatFact] = useState("");
 
-  Axios.get("https://catfact.ninja/fact").then((res) => {
-    setCatFact(res.data.fact);
-  });
+  useEffect(() => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  }, []);
+    
   return (
     <div className="App">
       <button> Generate Cat Fact </button>
--- myreactapp/src/index.js
+++ myreactapp/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = React.DOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode> removed to prevent fetching data twice used in useEffect.
+  <App/>
+  /* </React.StrictMode> */
 );
