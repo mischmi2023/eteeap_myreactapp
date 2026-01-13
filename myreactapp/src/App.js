1e807a64ab5d5b93820d814efe4e1bccc05833cd
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-13 20:15:23 +0800

    update App.js
    React Router DOM
    Routes in React
    components

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,28 +1,15 @@
 import "./App.css";
-import Axios from "axios";
-import { useState, useEffect } from "react";
+import { BrowseRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [generatedExcuse, setGeneratedExcuse] = useState("");
-
-  const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
-      (res) => {
-        setGeneratedExcuse(res.data[0].excuse);
-      }
-    );
-  };
-
   return (
     <div className="App">
-      <h1> Generate An Excuse </h1>
-      <button onClick={() => fetchExcuse("party")}> Party </button>
-      <button onClick={() => fetchExcuse("family")}> Family </button>
-      <button onClick={() => fetchExcuse("office")}> Office </button>
-
-      <p> {generatedExcuse} </p>
+      <Router> 
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
     </div>
-  );
- 
+)
 }
 export default App;
