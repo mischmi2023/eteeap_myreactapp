91c99965210b606758957f954260c8d1f03289f4
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-13 20:58:06 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Menu } from './pages/Menu';
-import { Contact } from './pages/Contact';
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
 
 function App() {
   return (
@@ -21,6 +22,6 @@ function App() {
         </Routes>
       </Router>
     </div>
-)
+  );
 }
 export default App;
