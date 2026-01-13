14559c01339a8c3bca0c4efe09eeac3e7f2ce015
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-13 20:15:58 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowseRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
