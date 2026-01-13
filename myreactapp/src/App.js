b84ce1c0baeaec6902cef766ae3890e7d9bf11d6
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-13 21:10:57 +0800

    Update App.js

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
+import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./Navbar";
 import { useState } from "react";
@@ -10,15 +10,11 @@ function App() {
   return (
     <div className="App">
       <Router> 
-        <div>
-          <Link to="/"> Home </Link>
-          <Link to="/Menu"> Menu </Link>
-          <Link to="/Contact"> Contact </Link>
-        </div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="Menu" element={<Menu />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<h1>Page not Found!</h1>} />
         </Routes>
       </Router>
