7b856e9c4b153539e39ce7da70d834bdc6b99fb9
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-13 20:48:37 +0800

    App.js
    Creating Links

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Menu } from './pages/Menu';
+import { Contact } from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <Router> 
+        <div>
+          <Link to="/"> Home </Link>
+          <Link to="/Menu"> Menu </Link>
+          <Link to="/Contact"> Contact </Link>
+        </div>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="Menu" element={<Menu />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<h1>Page not Found!</h1>} />
         </Routes>
       </Router>
     </div>
