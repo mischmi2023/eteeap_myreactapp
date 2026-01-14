b77511a34d9ecb75bf4d39279cfe046596faecee
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-14 18:47:20 +0800

    Create ChangeProfile.js
    create ChangeProfile.js

---
+++ myreactapp/src/components/ChangeProfile.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export const ChangeProfile = (props) => {
+    const [newUsername, setNewUsername] = useState("");
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={newUsername}
+                onChange={(event)} => {
+                    setNewUsername(event.target.value);
+                }}
+            />
+            <button
+                onClick={() => {
+                    props.setUsername(newUsername);
+                    setNewUsername("");
+                }}
+        </div>
+    );
+};
