c5e6db0a12862c126e0b6f0f9abe7f89d5d46aaf
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-14 19:53:41 +0800

    Update App.js
    UseContext Hook
    import createContext Hook and use it to have a global context for all the components to hold all the information pass as argument

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -4,25 +4,26 @@ import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./Navbar";
-import { useState } from "react";
+import { useState, createContext } from "react";
+
+export const AppContext = createContext();
 
 function App() {
   const [username, setUsername] = useState("Mike");
 
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home username={username} />} />
-          <Route 
-            path="/profile"
-            element={<Profile username={username} setUsername={setUsername} />}
-            />
+      <AppContext.Provider value={{ username, setUsername }}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<h1>Page not Found!</h1>} />
           </Routes>
         </Router>
+      </AppContext.Provider>
     </div>
   );
 }
