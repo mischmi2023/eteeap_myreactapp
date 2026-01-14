008934ab6ee06510d09b2ad0404989192eafb0d6
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-14 19:58:03 +0800

    Update Home.js

--- myreactapp/src/pages/Home.js
+++ myreactapp/src/pages/Home.js
@@ -1,3 +1,7 @@
-export const Home = (props) => {
-    return <h1> This is the Home Page and user is: {props.username}</h1>;
-}
+import { useContext } from "react";
+import { AppContext } from "../App";
+
+export const Home = () => {
+    const {username} = useContext(AppContext);
+    return <h1> This is the Home Page and user is: {username}</h1>;
+};
