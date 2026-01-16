d5b884de604e09422cc80c96506ecb221028a411
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-16 22:27:31 +0800

    Update App.js
    two independent toggling

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -3,11 +3,15 @@ import { useToggle } from "./useToggle";
 
 function App() {
   const [isVisible, toggle] = useToggle();
+  const [isVisible2, toggle2] = useToggle();
 
   return (
     <div className="App">
       <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
       {isVisible && <h1> Hidden Text </h1>}
+
+      <button onClick={toggle2}>{isVisible2 ? "Hide" : "Show"}</button>
+      {isVisible2 && <h1> Hidden Text </h1>}
     </div>
   );
 }
