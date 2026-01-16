e761fcf45f0d2bd975646fcbbc7476dd5e49d2ce
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-16 20:49:53 +0800

    Form.js
    adding validations from yup

--- myreactapp/src/components/Form.js
+++ myreactapp/src/components/Form.js
@@ -1,23 +1,39 @@
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
 export const Form = () => {
     const { register, handleSubmit } = useForm();
+
+    const schema = yup.object().shape({
+        fullName: yup.string().required(),
+        email: yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword: yup
+            .string()
+            .oneOf([yup.ref("password"), null])
+            .required(),
+    });
 
     const onSubmit = (data) => {
         console.log(data);
     };
 
-return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Full Name..." {...register("fullName")} /> 
-        <input type="text" placeholder="Email..." {...register("email")} />
-        <input type="number" placeholder="Age..." {...register("age")} />
-        <input
-            type="password"
-            placeholder="Password..."
-            {...register("password")}
-        />
-        <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")} />
-        <input type="Submit" />
-    </form>
- );
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Full Name..." {...register("fullName")} />
+            <input type="text" placeholder="Email..." {...register("email")} />
+            <input type="number" placeholder="Age..." {...register} />
+            <input
+                type="password"
+                placeholder="Password..."
+                {...register("password")}
+            />
+            <input
+                type="password"
+                placeholder="Confirm Password..."
+                {...register("confirmPassword")}
+            />
+            <input type="Submit" />
+        </form>
+    );
 };
