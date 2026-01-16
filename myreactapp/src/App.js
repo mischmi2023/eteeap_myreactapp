14fc320dc3ef33a5ef0c50c73c0301fbe0ef75ee
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-16 20:00:18 +0800

    App.js
    React Hook Form
    YUP Tutorial, and Forms

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,33 +1,12 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import { Navbar } from "./Navbar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Form } from "./components/Form.js";
 
-function App () {
-  // APP is the highest level component.
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-      refetchOnWindowFocus: false,
-      }
-    }});
-    return (
-      <div className="App">
-        <QueryClientProvider client={client}>
-          <Router>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/contact" elemt={< Contact />} />
-              <Route path="*" element={<h1>Page not Found!</h1>} />
-            </Routes>
-          </Router>
-        </QueryClientProvider>
-      </div>
-    );
+function App() {
+  return (
+    div className="App">
+    <Form /> 
+    
+    </div>
+  )
 }
 export default App;
