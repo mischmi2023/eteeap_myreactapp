0919f10f9b01fdd9d76d14c37aae560caed9c50e
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-16 19:52:29 +0800

    Home.js
    adding configuration to prevent updating of data whenever we switch window or tab

--- myreactapp/src/pages/Home.js
+++ myreactapp/src/pages/Home.js
@@ -1,16 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
 export const Home = () => {
-    const { data } = useQuery ({
+    const { data, isLoading, isError, refetch} = useQuery({
         queryKey: ["cat"],
         queryFn: () =>
             Axios.get("https://catfact.ninja/fact").then((res) => res.data),
     });
 
-    return (
-        <h1>
-            This is the Home Page
-            <p> {data?.fact} </p>
-        </h1>
-    );
+if (isError) {
+    return <h1> Sorry, an error ocurred. </h1>;
+}
+
+if (isLoading) {
+    return <h1> Loading... </h1>;
+}
+
+return (
+    <h1>
+        This is the Home Page
+        <p> {data?.fact} </p>
+        <button onClick = {refetch}> Update data </button>
+    </h1>
+ );
 };
