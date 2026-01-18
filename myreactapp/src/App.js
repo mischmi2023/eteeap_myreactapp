56a85902782fd473d65f3dc21e17f55aa39812f0
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-18 18:09:51 +0800

    App.js
    Array

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,16 +2,11 @@ import "./App.css";
 import { useToggle } from "./useToggle";
 
 function App() {
-  const [isVisible, toggle] = useToggle();
-  const [isVisible2, toggle2] = useToggle();
-
+  const { state, toggle } = useToggle(); // another example of returning an object
   return (
     <div className="App">
-      <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
-      {isVisible && <h1> Hidden Text </h1>}
-
-      <button onClick={toggle2}>{isVisible2 ? "Hide" : "Show"}</button>
-      {isVisible2 && <h1> Hidden Text </h1>}
+      <button onClick={toggle}>{state ? "Hide" : "Show"}</button>
+      {state && <h1> Hidden Text </h1>}
     </div>
   );
 }
