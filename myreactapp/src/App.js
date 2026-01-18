495d66aacc38c195dc30112d5b947e6aa88b14e0
mischmi2023 <mis.chmi2023@gmail.com>
2026-01-18 19:55:22 +0800

    App.js
    safety
    npm install prop-types

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 import { Person } from "./components/Person";
 
 function App() {
-    return (
-        <div className="App">
-            <Person
-                name="Mike"
-                email="mis.chmi2023@gmail.com"
-                age={41}
-                isMarried={false}
-                friends={["Karen", "Mabel", "Jerome", "Bianca", "Resty", "Meda"]}
-            />
-        </div>
-    );
+  return (
+    <div className="App"> 
+      <Person
+        name={9}
+        email="mis.chmi2023@gmail.com"
+        age={41}
+        isMarried={true}
+        friends={["Karen", "Mabel", "Jerome", "Bianca", "Resty", "Meda"]}
+      /> 
+    </div>
+  );
 }
 export default App;
